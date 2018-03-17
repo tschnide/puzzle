366bd278a4c1262eafc4b1adfe34da938ca4f5ee
Travis <Schnider@Traviss-MacBook-Air.local>
2018-03-17 14:29:59 -0600

    Frame out the class with methods from the assignment

--- src/Solver.java
+++ src/Solver.java
@@ -1,12 +1,29 @@
+import java.util.Iterator;
+
 public class Solver {
 
-    public Solver(Board b)
     public class Node{
         public Board b;
         public int moves;
         public Node previous;
     }
 
+    private Board board;
 
+    public Solver(Board initial){
+        board = initial;
+    }
+
+    public int moves(){
+
+    }
+
+    public Iterable<Board> solution(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
